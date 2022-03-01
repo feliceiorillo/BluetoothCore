7f403219c3e05ed3ce1dd045b04751940c5194e1
Felice Iorillo <felice.iorillo@outlook.it>
2022-03-01 18:35:24 +0100

    Add project files.

---
+++ BluetoothCore/Server.cs
@@ -0,0 +1,161 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace BluetoothCore
+{
+    public class Server
+    {
+        private static int clients = 0;
+        private static int _timeout = 30;
+        public static void Main()
+        {
+            TcpListener server = null;
+
+            IConfiguration config = new ConfigurationBuilder()
+                        .AddJsonFile("settings.json")
+                        .Build();
+
+            String ip = config.GetSection("ip").Value;
+            int port = Convert.ToInt32(config.GetSection("port").Value);
+            _timeout = Convert.ToInt32(config.GetSection("timeout").Value);
+
+            // Set the TcpListener on port 
+            IPAddress localAddr = IPAddress.Parse(ip);
+
+            // TcpListener server = new TcpListener(port);
+            server = new TcpListener(localAddr, port);
+
+            // Start listening for client requests.
+            server.Start();
+            Console.WriteLine($"Server started on {port}");
+
+            // we set up max client number
+            while (true)
+            {
+                if (clients >= 2)
+                    continue;
+
+                TcpClient client = server.AcceptTcpClient();
+                Console.WriteLine("A client connected.");
+                Thread clientThread = new Thread(new ParameterizedThreadStart(HandleComm));
+                clients += 1;
+                clientThread.Start(client);
+            
+            }
+        }
+
+        private static void HandleComm(object client)
+        { 
+
+                DateTime lastActiveConnection = DateTime.Now;
+                while (true)
+                {
+                    Console.WriteLine(clients);
+                    TcpClient tcpClient = (TcpClient)client;
+
+                    NetworkStream stream = tcpClient.GetStream();
+                    while (!stream.DataAvailable)
+                    {
+                        //if idle for more then 30 sec, close the connection
+                        if ((DateTime.Now - lastActiveConnection).Seconds > _timeout)
+                        {
+                            tcpClient.Close();
+                            Console.WriteLine("Closing thread for inactivity");
+                            Thread.CurrentThread.DisableComObjectEagerCleanup();
+                            clients -= 1;
+                            return;
+                        }
+                    };
+                    while (tcpClient.Available < 3) ; // match against "get"
+
+                    lastActiveConnection = DateTime.Now;
+                    byte[] bytes = new byte[tcpClient.Available];
+                    try
+                    {
+                        stream.Read(bytes, 0, tcpClient.Available);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                    string s = Encoding.UTF8.GetString(bytes);
+
+                    if (Regex.IsMatch(s, "^GET", RegexOptions.IgnoreCase))
+                    {
+                        Console.WriteLine("=====Handshaking from client=====\n{0}", s);
+
+                        // 1. Obtain the value of the "Sec-WebSocket-Key" request header without any leading or trailing whitespace
+                        // 2. Concatenate it with "258EAFA5-E914-47DA-95CA-C5AB0DC85B11" (a special GUID specified by RFC 6455)
+                        // 3. Compute SHA-1 and Base64 hash of the new value
+                        // 4. Write the hash back as the value of "Sec-WebSocket-Accept" response header in an HTTP response
+                        string swk = Regex.Match(s, "Sec-WebSocket-Key: (.*)").Groups[1].Value.Trim();
+                        string swka = swk + "258EAFA5-E914-47DA-95CA-C5AB0DC85B11";
+                        byte[] swkaSha1 = System.Security.Cryptography.SHA1.Create().ComputeHash(Encoding.UTF8.GetBytes(swka));
+                        string swkaSha1Base64 = Convert.ToBase64String(swkaSha1);
+
+                        // HTTP/1.1 defines the sequence CR LF as the end-of-line marker
+                        byte[] response = Encoding.UTF8.GetBytes(
+                            "HTTP/1.1 101 Switching Protocols\r\n" +
+                            "Connection: Upgrade\r\n" +
+                            "Upgrade: websocket\r\n" +
+                            "Sec-WebSocket-Accept: " + swkaSha1Base64 + "\r\n\r\n");
+
+                        stream.Write(response, 0, response.Length);
+                    }
+                    else
+                    {
+                        bool fin = (bytes[0] & 0b10000000) != 0,
+                            mask = (bytes[1] & 0b10000000) != 0; // must be true, "All messages from the client to the server have this bit set"
+
+                        int opcode = bytes[0] & 0b00001111, // expecting 1 - text message
+                            msglen = bytes[1] - 128, // & 0111 1111
+                            offset = 2;
+
+                        if (msglen == 126)
+                        {
+                            // was ToUInt16(bytes, offset) but the result is incorrect
+                            msglen = BitConverter.ToUInt16(new byte[] { bytes[3], bytes[2] }, 0);
+                            offset = 4;
+                        }
+                        else if (msglen == 127)
+                        {
+                            Console.WriteLine("TODO: msglen == 127, needs qword to store msglen");
+                            // i don't really know the byte order, please edit this
+                            // msglen = BitConverter.ToUInt64(new byte[] { bytes[5], bytes[4], bytes[3], bytes[2], bytes[9], bytes[8], bytes[7], bytes[6] }, 0);
+                            // offset = 10;
+                        }
+
+                        if (msglen == 0)
+                            Console.WriteLine("msglen == 0");
+                        else if (mask)
+                        {
+                            byte[] decoded = new byte[msglen];
+                            byte[] masks = new byte[4] { bytes[offset], bytes[offset + 1], bytes[offset + 2], bytes[offset + 3] };
+                            offset += 4;
+
+                            for (int i = 0; i < msglen; ++i)
+                                decoded[i] = (byte)(bytes[offset + i] ^ masks[i % 4]);
+
+                            string text = Encoding.UTF8.GetString(decoded);
+                            Console.WriteLine("{0}", text);
+                        }
+                        else
+                            Console.WriteLine("mask bit not set");
+
+                        Console.WriteLine();
+                    }
+                }
+
+            
+           
+
+        }
+    }
+}
